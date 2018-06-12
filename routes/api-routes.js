c5f5b134bc715b876ae8904024e7ec493e5b4cf6
Colten Krieg <coltenkrieg@Coltens-MacBook-Pro.local>
2018-06-12 16:29:27 -0700

    Stashing changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,7 +13,7 @@ var db = require("../models");
 module.exports = function(app) {
 
   // GET route for getting all of the posts
-  app.get("/api/posts/", function(req, res) {
+  app.get("/api/people/", function(req, res) {
     db.Post.findAll({})
       .then(function(dbPost) {
         res.json(dbPost);
@@ -21,10 +21,10 @@ module.exports = function(app) {
   });
 
   // Get route for returning posts of a specific category
-  app.get("/api/posts/category/:category", function(req, res) {
+  app.get("/api/posts/name/:name", function(req, res) {
     db.Post.findAll({
       where: {
-        category: req.params.category
+        name: req.params.name
       }
     })
       .then(function(dbPost) {
@@ -33,7 +33,7 @@ module.exports = function(app) {
   });
 
   // Get route for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
+  app.get("/api/people/:id", function(req, res) {
     db.Post.findOne({
       where: {
         id: req.params.id
