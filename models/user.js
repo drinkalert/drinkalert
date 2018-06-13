ea86bf8fcfdab6c852f50ffb0d10669d399390c2
Jose Sahagun <JoseSahagun@Joses-MacBook-Pro-2.local>
2018-06-12 18:45:36 -0700

    adding foreign keys

--- models/user.js
+++ models/user.js
@@ -19,5 +19,15 @@ module.exports = function (seqelize, DataTypes) {
             defaultValue: 1,
         }
     });
+
+    Post.associate = function(models) {
+        // We're saying that Drink should belong to a User
+        // A Drink can't be created without an User due to the foreign key constraint
+        Post.belongsTo(models.Drink, {
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      };
     return User
 }
